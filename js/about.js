292d9e57119efbd0bbe1b027b8ae2276b63c6e7e
mypletscode7 <elroydupreez69@gmail.com>
2026-08-02 02:30:43 +0200

    Implement word animation in about.js
    Add animation effect to words in about section

--- js/about.js
+++ js/about.js
@@ -1,1 +1,35 @@
+const text =
+document.getElementById("about-text");
 
+
+const words =
+text.innerHTML.split(" ");
+
+
+text.innerHTML="";
+
+
+words.forEach((word,index)=>{
+
+
+let span=document.createElement("span");
+
+
+span.innerHTML=word+" ";
+
+
+span.style.opacity=0;
+
+
+span.style.display="inline-block";
+
+
+span.style.animation=
+`wordAppear 0.5s forwards ${index*0.08}s`;
+
+
+
+text.appendChild(span);
+
+
+});
